9d4e6616e851916477923f1ebe9aa8fe361534af
Sayanth789 <sayanthstrng@gmail.com>
2025-09-26 19:12:34 +0530

    Adding the contianer to the app

--- Flask Messages & Advanced Validations/run.py
+++ Flask Messages & Advanced Validations/run.py
@@ -2,7 +2,6 @@ from market import app
 
 # Cheks if the run.py file has executed directly and not imported.
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
-    
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
