035b93bd3b78174daaaf41adda83a8ad2d107b50
Sayanth789 <sayanthstrng@gmail.com>
2025-09-26 20:16:27 +0530

    Adding container to the second app

--- flask_Validation/run.py
+++ flask_Validation/run.py
@@ -3,5 +3,5 @@ from market import app
 # Checks if the run.py file has executed ditectly and not imported.
 
 if __name__ ==  '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
     
